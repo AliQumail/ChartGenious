90ec57f821f5cac51dd27b9b93fda708249d15a6
Ali Qumail <aliqumail8@gmail.com>
2024-03-03 21:21:38 +0500

    FEAT - Added sort and select top results functionality in bar chart

--- src/app/components/charts/bar-chart/bar-chart.component.ts
+++ src/app/components/charts/bar-chart/bar-chart.component.ts
@@ -18,9 +18,12 @@ export class BarChartComponent {
  chartHeight = GlobalConstants.CHART_HEIGHT;
  chartWidth = GlobalConstants.CHART_WIDTH;
 
- column1: number = -1; 
- column2: number = -1; 
- column3: number = -1; 
+ column1: string = ''; 
+ column2: string = ''; 
+ sortType: number = 0; // No sort is 0, ascending is 1, descending in -1 
+ trimRecords = 0; 
+ newLength = 0; 
+
 
   public barChartLegend = true;
   public barChartPlugins = [];
@@ -29,7 +32,6 @@ export class BarChartComponent {
     labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
     datasets: [
       { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
     ]
   };
 
@@ -37,17 +39,25 @@ export class BarChartComponent {
     responsive: false,
   };
 
-  onChangeSelectColumn(event: any, columnNo: number){
+  onChangeSelectColumn(event: any, dropdownNo: number){
     let value = event.target.value; 
-    if (columnNo == 0) this.column1 = value;
-    if (columnNo == 1) this.column2 = value;
-    if (columnNo == 2) this.column3 = value;
+    if (dropdownNo == 0) this.column1 = value;
+    else if (dropdownNo == 1) this.column2 = value;
+    else if (dropdownNo == 2) this.sortType = value;
+    else if (dropdownNo == 3) this.trimRecords = value;
    
-    if (this.column1 != -1 && this.column2 != -1 && this.column3 != -1){
-      let labels = this.data[this.column1];      
+    let tempData = this.data; 
+    if (this.column1 != '' && this.column2 != ''){
+      tempData = this.sortData(tempData, this.sortType, this.column2);
+      if (this.trimRecords == 0) {
+        this.newLength = this.data.length; 
+      } else {
+        this.newLength = this.trimRecords;
+      }
+      let labels = tempData.slice(0, this.newLength).map( (d: any) => d[this.column1]);     
       let datasets = [
-        { data: this.data[this.column2], label: this.columns[this.column2] },
-        { data: this.data[this.column3], label: this.columns[this.column3] }
+        { data: tempData.slice(0, this.newLength).map( (d: any) => d[this.column2]), 
+        },
       ]
       this.barChartData = {
         labels: labels,
@@ -55,6 +65,17 @@ export class BarChartComponent {
       };
     }
     
+  }
+
+  sortData(data: any, sortType: number, columnName: string){
+    if (sortType == 1) {
+      data.sort((a: any, b: any) => a[columnName] - b[columnName])
+    } else if (sortType == -1) {
+      data.sort( (a: any, b: any) => b[columnName] - a[columnName]);
+    } else {
+      data = this.data; 
+    }
+    return data; 
   }
 
 }
